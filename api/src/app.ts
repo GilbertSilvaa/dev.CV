c7028e92fd7ba3ec5ece501efbe5ce0e582c70d9
Gilbert  Santos <gilbertkemell@hotmail.com>
2023-05-31 00:16:07 -0300

    feat: server port preparation for production and co cors configuration

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import { userRoutes } from './routes/user-routes';
 import { loginRoutes } from './routes/login-routes';
@@ -10,6 +11,9 @@ import { educationRoutes } from './routes/education-routes';
 
 export const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
 
 app.use('/', loginRoutes);
 app.use('/user', userRoutes);
--- api/src/server.ts
+++ api/src/server.ts
@@ -3,9 +3,11 @@ import { config } from 'dotenv';
 import { app } from './app';
 
 config();
+const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
+
 connect(process.env.MONGODB_CONNECTION!)
 .then(() => {
   console.log('connected db');
-  app.listen(5000, () => console.log('server running...'));
+  app.listen(PORT, () => console.log('server running...'));
 })
 .catch(error => console.log('error in server start: ', error));
